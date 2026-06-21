b17bc54aae4c50cd286e1b863797933dd8487041
Gouranga Ghosh <88451025+gourangabwn98@users.noreply.github.com>
2026-06-22 03:31:57 +0530

    login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -219,7 +219,7 @@ useEffect(() => {
         {step === "phone" ? (
           <>
             <div style={{ color: WHITE, fontWeight: 600, fontSize: 16, marginBottom: 20 }}>
-              LogIn
+              Log In
             </div>
 
             {/* Chef / Waiter dropdown */}
